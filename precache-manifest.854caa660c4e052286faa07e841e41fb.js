493a57c0f28a26acde3d37d8a37ce6f0d16c6b05
Yadhu Krishnan <yadhuk94@gmail.com>
2020-06-13 18:59:31 +0530

    Updates

---
+++ precache-manifest.854caa660c4e052286faa07e841e41fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c334a404d594fed6c4564badb12f181f",
+    "url": "/pizza1/index.html"
+  },
+  {
+    "revision": "573ed1186a0cf4126cd7",
+    "url": "/pizza1/static/css/main.54636f10.chunk.css"
+  },
+  {
+    "revision": "0cedb0ce819cd5c6acdc",
+    "url": "/pizza1/static/js/2.48bb8971.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/pizza1/static/js/2.48bb8971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "573ed1186a0cf4126cd7",
+    "url": "/pizza1/static/js/main.d5137365.chunk.js"
+  },
+  {
+    "revision": "8fd4d2ebce92d68b683f",
+    "url": "/pizza1/static/js/runtime-main.4a71130f.js"
+  }
+]);
